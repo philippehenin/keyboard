94bd7d971ebc0360d55a2ebd02cca4bb1bf0da81
philippehenin <philippehenin@users.noreply.github.com>
2019-03-19 14:42:28 +0100

    Update zlant_phe_keymap.c
    switch shift ctrl on the left
    switch quote shift enter on the right

--- zlant_phe_keymap.c
+++ zlant_phe_keymap.c
@@ -8,9 +8,9 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BL] = LAYOUT(
     KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-    KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-    KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
-    KC_LSFT, KC_LALT, KC_LGUI, KC_LSFT, MO(_AL), KC_SPC,  KC_SPC,  MO(_FL), KC_LBRC, KC_RBRC, KC_BSLS, KC_RSFT
+    KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+    KC_LCTL, KC_LALT, KC_LGUI, KC_LSFT, MO(_AL), KC_SPC,  KC_SPC,  MO(_FL), KC_LBRC, KC_RBRC, KC_BSLS, KC_QUOT
   ),
 
     [_AL] = LAYOUT(
@@ -34,6 +34,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     ),
 };
-
-
-
